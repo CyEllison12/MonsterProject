c921c258dbe7188c3134cc02e852b1298b499cd1
Cy Ellison <dragonz2626@gmail.com>
2018-09-11 12:13:01 -0600

    made monster controller and other things

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,23 @@
 package controller;
 
+import model.MarshmallowMonster;
+
 public class Controller
 {
+	//Data member section
+	//Declaration section
+	//ALWAY PRIVATE!!!!
+	private MarshmallowMonster myMonster;
+	
+	//Constructors initialize data members!
+	public Controller()
+	{
+		myMonster = new MarshmallowMonster();
+	}
+	
 	public void start()
 	{
+		System.out.println(myMonster);
 		
 	}
 }
