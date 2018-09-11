ebc1d32df4ec4f48e9d818d188e3fa43bb92bf17
Cy Ellison <dragonz2626@gmail.com>
2018-09-11 13:03:01 -0600

    Displays the info i put in

--- src/model/MarshmallowMonster.java
+++ src/model/MarshmallowMonster.java
@@ -33,7 +33,13 @@ public class MarshmallowMonster
 	
 	public String toString()
 	{
-		String description = "";
+		String description = "This monster is named: " + name
+							+ "and it has " + legCount + " legs...."
+							+ "and it has " + eyeCount + " eyes...."
+							+ "and it has " + armCount + " arms...."
+							+ "does he have a nose? " + hasNoses
+							+ "His favorite thing to say is:" 
+							+ "F-R-E-E That spells free, FreeCreditReport.com baby!";
 		
 		return description;
 	}
